09b6a6442bd3ceefeeeb83317efc86d3a1a89baa
ihorman <ihorman@gmail.com>
2016-05-31 13:36:40 +0200

    Update start.sh

--- payara-micro-cluster/start.sh
+++ payara-micro-cluster/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 PORT=${PORT:-80}
+HAZELCAST_CONFIG="/opt/config/hazelcast.xml";
+
+sed -i "s@GROUPNAME@${HAZELCAST_GROUP}@g" $HAZELCAST_CONFIG;
+sed -i "s@PASSWORD@${HAZELCAST_PASSWORD}@g" $HAZELCAST_CONFIG;
 
 java -jar /opt/payara-micro.jar --port $PORT --deploymentDir /opt/payara-micro-wars --hzConfigFile /opt/config/hazelcast.xml
 
