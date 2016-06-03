8264ea66d7887bc838572060729aa56f87b57c3a
siruslan <siruslan@gmail.com>
2016-06-03 17:21:19 +0200

    updated vert scaling logic

--- payara-micro-cluster/bin/start.sh
+++ payara-micro-cluster/bin/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PORT=${PORT:-80}
+GC_PERIOD=${GC_PERIOD:-300}
 HAZELCAST_CONFIG="/opt/config/hazelcast.xml";
 
 sed -i "s@GROUPNAME@${HAZELCAST_GROUP}@g" $HAZELCAST_CONFIG;
@@ -8,7 +9,7 @@ sed -i "s@PASSWORD@${HAZELCAST_PASSWORD}@g" $HAZELCAST_CONFIG;
 
 source /opt/bin/memoryConfig.sh
 
-JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/lib/jelastic-gc-agent.jar"
+[ ! -z "$VERT_SCALING" ] && JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/lib/jelastic-gc-agent.jar=period=$GC_PERIOD"
 
 java -jar /opt/payara-micro.jar $JAVA_OPTS --port $PORT --deploymentDir /opt/payara-micro-wars --hzConfigFile /opt/config/hazelcast.xml 
 
