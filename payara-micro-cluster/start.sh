9f9ad4dfeef90e29682b375be97e2489d7b19442
siruslan <siruslan@gmail.com>
2016-06-03 13:28:32 +0200

    added JAVA_OPTS

--- payara-micro-cluster/start.sh
+++ payara-micro-cluster/start.sh
@@ -6,5 +6,5 @@ HAZELCAST_CONFIG="/opt/config/hazelcast.xml";
 sed -i "s@GROUPNAME@${HAZELCAST_GROUP}@g" $HAZELCAST_CONFIG;
 sed -i "s@PASSWORD@${HAZELCAST_PASSWORD}@g" $HAZELCAST_CONFIG;
 
-java -jar /opt/payara-micro.jar --port $PORT --deploymentDir /opt/payara-micro-wars --hzConfigFile /opt/config/hazelcast.xml
+java -jar /opt/payara-micro.jar $JAVA_OPTS --port $PORT --deploymentDir /opt/payara-micro-wars --hzConfigFile /opt/config/hazelcast.xml
 
