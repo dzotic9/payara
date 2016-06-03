cadb9d4dce890383cc1cef440367620a98f8d85c
Ihor Kolodyuk <ik@jelastic.com>
2016-06-03 15:26:34 +0200

    restructurisation

--- payara-micro-cluster/bin/start.sh
+++ payara-micro-cluster/bin/start.sh
@@ -8,5 +8,7 @@ sed -i "s@PASSWORD@${HAZELCAST_PASSWORD}@g" $HAZELCAST_CONFIG;
 
 source /opt/memoryConfig.sh
 
-java -jar /opt/payara-micro.jar $JAVA_OPTS --port $PORT --deploymentDir /opt/payara-micro-wars --hzConfigFile /opt/config/hazelcast.xml
+JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/lib/jelastic-gc-agent.jar"
+
+java -jar /opt/payara-micro.jar $JAVA_OPTS --port $PORT --deploymentDir /opt/payara-micro-wars --hzConfigFile /opt/config/hazelcast.xml 
 
