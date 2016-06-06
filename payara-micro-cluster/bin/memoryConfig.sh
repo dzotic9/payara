90e7406ac49f4988d55c8b083b2eca43ec41bde5
siruslan <siruslan@gmail.com>
2016-06-06 13:33:02 +0200

    optimal jvm setting fixes

--- payara-micro-cluster/bin/memoryConfig.sh
+++ payara-micro-cluster/bin/memoryConfig.sh
@@ -1,37 +1,67 @@
 #!/bin/bash        
 
-GC_LIMMIT=8000;
-[ -z "$XMS" ] && { XMS=32M; }
-[ -z "$XMN" ] && { XMN=30M; }
-memory_total=`free -m | grep Mem | awk '{print $2}'`;
-[ -z "$XMX" ] && {
-#optimal XMX = 90% * total available RAM
-#it differs from default values -Xmx http://docs.oracle.com/cd/E13150_01/jrockit_jvm/jrockit/jrdocs/refman/optionX.html
-            let XMX=memory_total*9/10;
-            XMX="${XMX}M";
-}
-[ -z "$GC" ] && {  [ "$XMX" -ge "$GC_LIMMIT" ] && GC="-XX:+UseG1GC" || GC="-XX:+UseParNewGC"; }
-
-if ! `echo $confresult | grep -q "\-Xminf[[:digit:]\.]"`
+if ! `echo $JAVA_OPTS | grep -q "\-Xms[[:digit:]\.]"`
 then
-    confresult=$confresult" -Xminf0.1"
+            [ -z "$XMS" ] && { XMS=32M; }
+            JAVA_OPTS=$JAVA_OPTS" $XMS"; 
 fi
 
-if ! `echo $confresult | grep -q "\-Xmaxf[[:digit:]\.]"`
+if ! `echo $JAVA_OPTS | grep -q "\-Xmn[[:digit:]\.]"`
 then
-           confresult=$confresult" -Xmaxf0.3"
+            [ -z "$XMN" ] && { XMN=30M; }
+            JAVA_OPTS=$JAVA_OPTS" $XMN"; 
+fi
+
+if ! `echo $JAVA_OPTS | grep -q "\-Xmx[[:digit:]\.]"`
+then
+            [ -z "$XMX" ] && {
+                        #optimal XMX = 80% * total available RAM
+                        #it differs a little bit from default values -Xmx http://docs.oracle.com/cd/E13150_01/jrockit_jvm/jrockit/jrdocs/refman/optionX.html
+                        memory_total=`free -m | grep Mem | awk '{print $2}'`;
+                        let XMX=memory_total*8/10;
+                        XMX="${XMX}M";
+            }
+            JAVA_OPTS=$JAVA_OPTS" $XMX";
+fi
+
+if ! `echo $JAVA_OPTS | grep -q "\-XX:+Use.*GC"`
+then
+            GC_LIMMIT=8000;
+            [ -z "$GC" ] && {  [ "$XMX" -ge "$GC_LIMMIT" ] && GC="-XX:+UseG1GC" || GC="-XX:+UseParNewGC"; }
+            JAVA_OPTS=$JAVA_OPTS" $GC"; 
+fi
+
+
+if ! `echo $JAVA_OPTS | grep -q "\-Xminf[[:digit:]\.]"`
+then
+            [ -z "$XMINF" ] && { XMINF=-Xminf0.1; }
+            JAVA_OPTS=$JAVA_OPTS" $XMINF"; 
+fi
+
+if ! `echo $JAVA_OPTS | grep -q "\-Xmaxf[[:digit:]\.]"`
+then
+            [ -z "$XMAXF" ] && { XMAXF=-Xmaxf0.3; }
+            JAVA_OPTS=$JAVA_OPTS" $XMAXF"; 
 fi
 
 # 1) we need to check version of java, if it's > 7 then ignore this parameter 
 # 2) otherwise to define MaxPermSize in optimal way (it should not be bigger than available RAM) 
-if ! `echo $confresult | grep -q "\-XX:MaxPermSize"`
+if ! `echo $JAVA_OPTS | grep -q "\-XX:MaxPermSize"`
 then
-            confresult=$confresult" -XX:MaxPermSize=256m "
+            [ -z "$MAXPERMSIZE" ] && { 
+                        let MAXPERMSIZE_VALUE=$XMX/10; 
+                        [ $MAXPERMSIZE_VALUE -ge 64 ] && {
+                                    MAXPERMSIZE="-XX:MaxPermSize=$MAXPERMSIZE_VALUE";
+                                    JAVA_OPTS=$JAVA_OPTS" $MAXPERMSIZE";
+                        }
+            }
+            JAVA_OPTS=$JAVA_OPTS" $MAXPERMSIZE";
 fi
    
-if ! `echo $confresult | grep -q "UseCompressedOops"`
+if ! `echo $JAVA_OPTS | grep -q "UseCompressedOops"`
 then
-            confresult=$confresult" -XX:+UseCompressedOops"
+            JAVA_OPTS=$JAVA_OPTS" -XX:+UseCompressedOops"
 fi
-export JAVA_OPTS=$confresult" $GC"
+
+export $JAVA_OPTS
 
