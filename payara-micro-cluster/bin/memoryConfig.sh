ff641685db401a4b30d915557e33d3b4d21adb13
siruslan <siruslan@gmail.com>
2016-06-06 18:58:21 +0200

    polishing

--- payara-micro-cluster/bin/memoryConfig.sh
+++ payara-micro-cluster/bin/memoryConfig.sh
@@ -2,13 +2,13 @@
 
 if ! `echo $JAVA_OPTS | grep -q "\-Xms[[:digit:]\.]"`
 then
-            [ -z "$XMS" ] && { XMS=32M; }
+            [ -z "$XMS" ] && { XMS="-Xms32M"; }
             JAVA_OPTS=$JAVA_OPTS" $XMS"; 
 fi
 
 if ! `echo $JAVA_OPTS | grep -q "\-Xmn[[:digit:]\.]"`
 then
-            [ -z "$XMN" ] && { XMN=30M; }
+            [ -z "$XMN" ] && { XMN="-Xmn30M"; }
             JAVA_OPTS=$JAVA_OPTS" $XMN"; 
 fi
 
@@ -19,7 +19,7 @@ then
                         #it differs a little bit from default values -Xmx http://docs.oracle.com/cd/E13150_01/jrockit_jvm/jrockit/jrdocs/refman/optionX.html
                         memory_total=`free -m | grep Mem | awk '{print $2}'`;
                         let XMX=memory_total*8/10;
-                        XMX="${XMX}M";
+                        XMX="-Xmx${XMX}M";
             }
             JAVA_OPTS=$JAVA_OPTS" $XMX";
 fi
@@ -27,19 +27,22 @@ fi
 if ! `echo $JAVA_OPTS | grep -q "\-XX:+Use.*GC"`
 then
             GC_LIMMIT=8000;
-            [ -z "$GC" ] && {  [ "$XMX" -ge "$GC_LIMMIT" ] && GC="-XX:+UseG1GC" || GC="-XX:+UseParNewGC"; }
+            XMX_VALUE=`echo $XMX | grep -o "[0-9]*"`;
+            XMX_UNIT=`echo $XMX | sed "s/-Xmx//g" | grep -io "g\|m"`;
+            [ $XMX_UNIT == "g" -o $XMX_UNIT == "G" ] && { let XMX_VALUE=$XMX_VALUE*1024; } 
+            [ -z "$GC" ] && {  [ "$XMX_VALUE" -ge "$GC_LIMMIT" ] && GC="-XX:+UseG1GC" || GC="-XX:+UseParNewGC"; }
             JAVA_OPTS=$JAVA_OPTS" $GC"; 
 fi
 
 if ! `echo $JAVA_OPTS | grep -q "\-Xminf[[:digit:]\.]"`
 then
-            [ -z "$XMINF" ] && { XMINF=-Xminf0.1; }
+            [ -z "$XMINF" ] && { XMINF="-Xminf0.1"; }
             JAVA_OPTS=$JAVA_OPTS" $XMINF"; 
 fi
 
 if ! `echo $JAVA_OPTS | grep -q "\-Xmaxf[[:digit:]\.]"`
 then
-            [ -z "$XMAXF" ] && { XMAXF=-Xmaxf0.3; }
+            [ -z "$XMAXF" ] && { XMAXF="-Xmaxf0.3"; }
             JAVA_OPTS=$JAVA_OPTS" $XMAXF"; 
 fi
 
@@ -48,13 +51,15 @@ then
             [ -z "$MAXPERMSIZE" ] && { 
                         JAVA_VERSION=$(java -version 2>&1 | grep version |  awk -F '.' '{print $2}')
                         # if java version <= 7 then congigure MaxPermSize otherwise ignore 
-                        [ $JAVA_VERSION -le 7 ] && {
-                            let MAXPERMSIZE_VALUE=$XMX/10; 
-                            [ $MAXPERMSIZE_VALUE -ge 64 ] && {
-                                        [ $MAXPERMSIZE_VALUE -gt 256 ] && { MAXPERMSIZE_VALUE=256; }
-                                        MAXPERMSIZE="-XX:MaxPermSize=${MAXPERMSIZE_VALUE}M";
-                                     JAVA_OPTS=$JAVA_OPTS" $MAXPERMSIZE";
-                             }
+                        [ $JAVA_VERSION -le 8 ] && {
+                                    XMX_VALUE=`echo $XMX | grep -o "[0-9]*"`
+                                    XMX_UNIT=`echo $XMX | sed "s/-Xmx//g" | grep -io "g\|m"`;
+            	            [ $XMX_UNIT == "g" -o $XMX_UNIT == "G" ] && { let XMX_VALUE=$XMX_VALUE*1024; } 
+		            let MAXPERMSIZE_VALUE=$XMX_VALUE/10; 
+                                    [ $MAXPERMSIZE_VALUE -ge 64 ] && {
+                                                [ $MAXPERMSIZE_VALUE -gt 256 ] && { MAXPERMSIZE_VALUE=256; }
+                                                MAXPERMSIZE="-XX:MaxPermSize=${MAXPERMSIZE_VALUE}M";
+                                    }
                         }
             }
             JAVA_OPTS=$JAVA_OPTS" $MAXPERMSIZE";
@@ -65,5 +70,4 @@ then
             JAVA_OPTS=$JAVA_OPTS" -XX:+UseCompressedOops"
 fi
 
-export $JAVA_OPTS
-
+export JAVA_OPTS
