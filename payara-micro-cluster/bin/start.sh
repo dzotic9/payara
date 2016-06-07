9a54f7bd15bf6cc131f2668231f104ff6b5d470a
siruslan <siruslan@gmail.com>
2016-06-07 14:01:10 +0200

    fixed JAVA_OPTS order

--- payara-micro-cluster/bin/start.sh
+++ payara-micro-cluster/bin/start.sh
@@ -2,6 +2,7 @@
 
 PORT=${PORT:-80}
 GC_PERIOD=${GC_PERIOD:-300}
+GC_DEBUG=${GC_DEBUG:-0}
 HAZELCAST_CONFIG="/opt/config/hazelcast.xml";
 
 sed -i "s@GROUPNAME@${HAZELCAST_GROUP}@g" $HAZELCAST_CONFIG;
@@ -9,7 +10,7 @@ sed -i "s@PASSWORD@${HAZELCAST_PASSWORD}@g" $HAZELCAST_CONFIG;
 
 source /opt/bin/memoryConfig.sh
 
-[ ! -z "$VERT_SCALING" -a "$VERT_SCALING" != "false" -a "$VERT_SCALING" != "0" ] && JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/lib/jelastic-gc-agent.jar=period=$GC_PERIOD"
+[ ! -z "$VERT_SCALING" -a "$VERT_SCALING" != "false" -a "$VERT_SCALING" != "0" ] && JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/lib/jelastic-gc-agent.jar=period=$GC_PERIOD,debug=$GC_DEBUG"
 
-java -jar /opt/payara-micro.jar $JAVA_OPTS --port $PORT --deploymentDir /opt/payara-micro-wars --hzConfigFile /opt/config/hazelcast.xml 
+java $JAVA_OPTS -jar /opt/payara-micro.jar --port $PORT --deploymentDir /opt/payara-micro-wars --hzConfigFile /opt/config/hazelcast.xml 
 
