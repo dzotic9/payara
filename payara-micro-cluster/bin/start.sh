be871501594013592d64355852baed61a7cb7223
siruslan <siruslan@gmail.com>
2016-07-07 18:01:27 +0200

    fixing path change

--- payara-micro-cluster/bin/start.sh
+++ payara-micro-cluster/bin/start.sh
@@ -3,14 +3,14 @@
 PORT=${PORT:-80}
 GC_PERIOD=${GC_PERIOD:-300}
 GC_DEBUG=${GC_DEBUG:-0}
-HAZELCAST_CONFIG="/opt/config/hazelcast.xml";
+HAZELCAST_CONFIG="$PAYARA_PATH/config/hazelcast.xml";
 
 sed -i "s@GROUPNAME@${HAZELCAST_GROUP}@g" $HAZELCAST_CONFIG;
 sed -i "s@PASSWORD@${HAZELCAST_PASSWORD}@g" $HAZELCAST_CONFIG;
 
-source /opt/bin/memoryConfig.sh
+source $PAYARA_PATH/bin/memoryConfig.sh
 
-[ ! -z "$VERT_SCALING" -a "$VERT_SCALING" != "false" -a "$VERT_SCALING" != "0" ] && JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/lib/jelastic-gc-agent.jar=period=$GC_PERIOD,debug=$GC_DEBUG"
+[ ! -z "$VERT_SCALING" -a "$VERT_SCALING" != "false" -a "$VERT_SCALING" != "0" ] && JAVA_OPTS="$JAVA_OPTS -javaagent:$PAYARA_PATH/lib/jelastic-gc-agent.jar=period=$GC_PERIOD,debug=$GC_DEBUG"
 
-java $JAVA_OPTS -jar /opt/payara-micro.jar --port $PORT --deploymentDir /opt/payara-micro-wars --hzConfigFile /opt/config/hazelcast.xml 
+java $JAVA_OPTS -jar $PAYARA_PATH/payara-micro.jar --port $PORT --deploymentDir $PAYARA_PATH/deployments --hzConfigFile $PAYARA_PATH/config/hazelcast.xml 
 
