7904094cbd36ccce871eaab96131aa7e936a7ada
siruslan <siruslan@gmail.com>
2016-07-07 22:35:52 +0200

    fixed gc agent injection condition

--- payara-micro-cluster/bin/start.sh
+++ payara-micro-cluster/bin/start.sh
@@ -10,7 +10,7 @@ sed -i "s@PASSWORD@${HAZELCAST_PASSWORD}@g" $HAZELCAST_CONFIG;
 
 source $HOME/bin/memoryConfig.sh
 
-[ ! -z "$VERT_SCALING" -a "$VERT_SCALING" != "false" -a "$VERT_SCALING" != "0" ] && JAVA_OPTS="$JAVA_OPTS -javaagent:$HOME/lib/jelastic-gc-agent.jar=period=$GC_PERIOD,debug=$GC_DEBUG"
+[ "$VERT_SCALING" != "false" -a "$VERT_SCALING" != "0" ] && JAVA_OPTS="$JAVA_OPTS -javaagent:$HOME/lib/jelastic-gc-agent.jar=period=$GC_PERIOD,debug=$GC_DEBUG"
 
 java $JAVA_OPTS -jar $HOME/payara-micro.jar --port $PORT --deploymentDir $HOME/deployments --hzConfigFile $HOME/config/hazelcast.xml 
 
