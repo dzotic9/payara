4ac5c3565697393ba8ea98aec8d6934afe315c6e
Ruslan Synytsky <siruslan@gmail.com>
2016-09-08 18:18:17 +0200

    Update install-jps.js

--- multi-jps-solutions/install-jps.js
+++ multi-jps-solutions/install-jps.js
@@ -4,7 +4,9 @@
 import com.hivext.api.core.utils.Transport;
 
 var envName = '${env.envName}';
-var url = "https://"+window.location.host.replace("app.", "appstore.")+"/installapp?manifest="+jps+"&shortdomain="+envName+"&targetAppid="+envName+"&session="+session;
-var resp = new Transport().get(url);
+//var url = "https://"+window.location.host.replace("app.", "appstore.")+"/installapp?manifest="+jps+"&shortdomain="+envName+"&targetAppid="+envName+"&session="+session;
+//var resp = new Transport().get(url);
 
-return {result: 0, url: url, resp: eval(resp)};
+//return {result: 0, url: url, resp: eval(resp)};
+
+return {result: 0, env: envName, jps: jps}
