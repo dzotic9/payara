ec4d0b93f66967ed6b78ba4c35550b524a793919
Ruslan Synytsky <siruslan@gmail.com>
2016-09-08 16:09:10 +0200

    Create install-jps.js

---
+++ multi-jps-solutions/install-jps.js
@@ -0,0 +1,10 @@
+//@auth
+//@req(jps)
+
+import com.hivext.api.core.utils.Transport;
+
+var envName = '${env.envName}';
+var url = "https://"+window.location.host.replace("app.", "appstore.")+"/installapp?manifest="+jps+"&shortdomain="+envName+"&targetAppid="+envName;
+var resp = new Transport().get(url);
+
+return {result: 0, url: url, resp: resp};
