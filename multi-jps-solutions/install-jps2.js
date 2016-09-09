3555cac6870be8b916101ebe383c6ae52836789d
Ruslan Synytsky <siruslan@gmail.com>
2016-09-09 13:50:26 +0200

    Update install-jps2.js

--- multi-jps-solutions/install-jps2.js
+++ multi-jps-solutions/install-jps2.js
@@ -1,23 +1,17 @@
 //@auth
 //@req(jps)
 
-import com.hivext.api.core.utils.Transport;
+import com.hivext.api.development.Scripting;
 
-var envName = '${env.envName}';
-var envAppid = '${env.appid}';
-var url = "https://"+window.location.host.replace("app.", "appstore.")+"/installapp";
+var scripting = new Scripting({
+    serverUrl : "http://" + window.location.host.replace("app.", "appstore.") + "/"
+});
 
-var params = {
-  manifest: jps,
-  shortdomain: envName, 
-  targetAppid: envAppid,
-  session: session
-}
-var resp = new Transport().request(url, params, window.location.headers, new com.jelastic.api.Response().getClass());
-
-return {result: -1, response: {resp: resp}};
-
-//window.location.href = url;
-//return {result: -1, response: {transport: transport, location : window.location}};
-
-//return {result: 0, response: {resp: eval(resp), url: url}};
+return scripting.eval({
+    script : "InstallApp",
+    session : session,
+    params : {
+        manifest : jps,
+        targetAppid : '${env.appid}',
+    }
+}).response;
