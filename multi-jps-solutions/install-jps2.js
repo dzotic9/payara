dae859f8ef20189c6098c1e52908b5869686eb5a
Ruslan Synytsky <siruslan@gmail.com>
2016-09-09 13:43:41 +0200

    Create install-jps2.js

---
+++ multi-jps-solutions/install-jps2.js
@@ -0,0 +1,23 @@
+//@auth
+//@req(jps)
+
+import com.hivext.api.core.utils.Transport;
+
+var envName = '${env.envName}';
+var envAppid = '${env.appid}';
+var url = "https://"+window.location.host.replace("app.", "appstore.")+"/installapp";
+
+var params = {
+  manifest: jps,
+  shortdomain: envName, 
+  targetAppid: envAppid,
+  session: session
+}
+var resp = new Transport().request(url, 'post', params, window.location.headers)
+
+return {result: -1, response: {resp: resp}};
+
+//window.location.href = url;
+//return {result: -1, response: {transport: transport, location : window.location}};
+
+//return {result: 0, response: {resp: eval(resp), url: url}};
