ced539ce985ce690eabf0f09878fbfb5bc4db39c
Ihor Kolodyuk <ik@jelastic.com>
2017-10-20 14:17:09 +0300

    payara cluster based on multicast  is removed

--- payara-micro-cluster-multicast/multicast-vtun-bridge/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ifconfig br0 10.0.0.1/24
-/etc/init.d/isc-dhcp-server start
-/etc/init.d/vtun start
-
--- payara-micro-cluster-multicast/payara-micro-cluster/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/etc/init.d/vtun start
-
-PORT=${PORT:-80}
-
-java -jar /opt/payara-micro.jar --port $PORT --deploymentDir /opt/payara-micro-wars
-
